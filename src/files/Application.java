54f1bb9f4eb618ded1d8b552050a3628eebea165
Igor <igorlipko75@gmail.com>
2018-06-02 12:18:53 +0300

    Application change

--- src/files/Application.java
+++ src/files/Application.java
@@ -3,6 +3,6 @@ package files;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("By, world!");
     }
 }
