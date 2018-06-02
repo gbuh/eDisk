6364c8e95d99e2ad2e841c111ec3d5978bc5c52c
Igor <igorlipko75@gmail.com>
2018-06-02 12:31:03 +0300

    Application refactoring

--- src/files/Application.java
+++ src/files/Application.java
@@ -1,8 +1,19 @@
 package files;
 
 public class Application {
+    private String nameApp;
+
+    public String getNameApp() {
+        return nameApp;
+    }
+
+    public void setNameApp(String nameApp) {
+        this.nameApp = nameApp;
+    }
 
     public static void main(String[] args) {
-        System.out.println("By, world!");
+        Application app = new Application();
+        app.setNameApp("Hi! I'm good app)");
+        System.out.println(app.getNameApp());
     }
 }
